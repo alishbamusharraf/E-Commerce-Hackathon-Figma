8d0a5d65449ae5e1b4f6eaef0e546350ffd6ac1f
Muhammad Annas <ma2259050@gmail.com>
2024-12-08 23:56:57 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Header from "../app/components/header"
+import Footer from "./components/footer"
+import HeroSection from "./components/heroSection"
+import RelatedProducts from "./components/relatedProducts"
+import BrandSection from "./components/brandSection"
+import ShoppingCart from "./components/shoppingCard"
+import DownHeader from "./components/downHeader";
+import BrandBuilt from "./components/brandbuilt"
+import FeatureFF from "./components/featureSection"
+import FeatureSecond from "./components/featureSection2";
+import Signup from "./components/signUp"
+import HeroSec from "./components/herosec";
+import View from "./components/viewproduct";
+import ProductSingle from "./components/productssingle"
+export default function Home() {
+  return (
+     <>
+
+     <Header/>
+     <DownHeader/>
+     <HeroSec/>
+     <View/>
+     <HeroSection/>
+     <RelatedProducts/>
+     <BrandSection/>
+     <ShoppingCart/>
+    
+     <BrandBuilt/>
+     <FeatureFF/>
+     <FeatureSecond/>
+     <Signup/>
+     <Footer/>
+
+     <ProductSingle/>
+     
+     </>
+  );
+}
